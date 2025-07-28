25fd1161216680c8c0bea0a9311c9f687e5aa508
yahya <ymua0001@student.monash.edu>
2025-07-28 14:15:20 +1000

    Added W1 Karatsuba implementation

---
+++ Algorithms/karatsuba.py
@@ -0,0 +1,21 @@
+import math
+def karatsuba(x, y):
+    if x < 10 and y < 10:
+        return x*y
+    else:
+        k = math.floor(math.log10(x)+1)
+        xl = x % (math.pow(10, k//2))
+        yl = y % (math.pow(10, k//2))
+        xm = (x - xl) // math.pow(10, k//2)
+        ym = (y - yl) // math.pow(10, k//2) 
+        a = karatsuba(xm, ym)
+        b = karatsuba(xl, yl)
+        c = karatsuba(xm + xl, yl + ym) - a - b
+        return a * math.pow(10, k) + c * math.pow(10, k//2) + b
+
+
+x = 1531
+y = 3453
+res = karatsuba(x,y)
+assert res == x*y
+print(res)
